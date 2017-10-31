e5b874bda2bc0e6b100ae513ba0ced4c02d352e8
nisidagod <32625692+nisidagod@users.noreply.github.com>
2017-10-31 10:57:35 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>こちらはLINE Messaging APIのデモサイトです。</title>
+<title>タイトル</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-こちらはLINE Messaging APIのデモサイトです。
+LINE Messaging APIのデモサイトです。
 </body>
 </html>
